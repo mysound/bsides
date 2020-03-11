60cf5902f843fb5da5c7cc3b2fb1e7ba81f854b7
unknown <mysound2015@yandex.ru>
2020-03-11 13:45:45 +0300

    Admin1103

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->boolean('admin')->default(0);
             $table->timestamps();
         });
     }
