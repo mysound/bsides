0b1440c9cd57e18bb87c64bf127133b154282d80
unknown <mysound2015@yandex.ru>
2020-07-10 22:42:09 +0300

    1007metrika

--- resources/views/layouts/app_store.blade.php
+++ resources/views/layouts/app_store.blade.php
@@ -14,6 +14,21 @@
     {!! NoCaptcha::renderJs() !!}
 </head>
 <body>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(34013765, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+       });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/34013765" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <div id="grid-page">
         @include('layouts.header_store')
         @yield('content')
